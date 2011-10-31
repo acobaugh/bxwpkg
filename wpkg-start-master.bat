36fa5a78a1ff1edb8d1e6f587253294413c3fa99
Andy Cobaugh <phalenor@gmail.com>
2011-10-30 22:09:05 -0400

    specify SOFTWARE env in wpkg-start-master.bat

--- wpkg-start-master.bat
+++ wpkg-start-master.bat
@@ -1,1 +1,3 @@
-cscript \\wpkg\wpkg\.svc-wpkg\master\wpkg.js /synchronize /quiet /nonotify
+set SOFTWARE=\\wpkg\wpkg
+
+start /wait "wpkg" cscript \\wpkg\wpkg\.svc-wpkg\master\wpkg.js /synchronize /quiet /nonotify
