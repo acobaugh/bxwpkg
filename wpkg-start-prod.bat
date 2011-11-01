599d1cf0c2598d68ac369be96582e34c32f1983b
Andy Cobaugh <phalenor@gmail.com>
2011-11-01 12:51:26 -0400

    scripts: don't use start command, maybe that will fix it?

--- wpkg-start-prod.bat
+++ wpkg-start-prod.bat
@@ -1,5 +1,3 @@
 set SOFTWARE=\\wpkg\wpkg
 
-ECHO %SOFTWARE%
-
-start /wait "wpkg" cscript \\wpkg\wpkg\.svc-wpkg\prod\wpkg.js /synchronize /quiet /nonotify
+cscript \\wpkg\wpkg\.svc-wpkg\prod\wpkg.js /synchronize /quiet /nonotify
